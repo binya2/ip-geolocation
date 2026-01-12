11fcf1a224d16cea6c57ae56b57eac93141c11ed
Yosef shoval <ys0542522504@gmail.com>
2026-01-12 17:31:10 +0200

    feat: cleaning data, main file run the ruotes

--- service-a/app/services.py
+++ service-a/app/services.py
@@ -7,12 +7,27 @@ from schemas import ipv4_address
 
 load_dotenv()
 
-API_KEY = os.getenv('API_KEY')
+API_KEY = os.getenv('API_KEY', None)
 ip2loc_url = 'https://api.ip2loc.com'
+service_b_url = 'http://'
+
+
 
 
 def clean_data(data: dict) -> dict:
-    pass
+    ip = data.get('connection').get('ip')
+    location = data.get('location')
+    latitude = location.get('latitude')
+    longitude = location.get('longitude')
+
+    cleaned_data = {
+        'ip': ip,
+        'latitude': latitude,
+        'longitude': longitude
+        }
+
+    return cleaned_data
+
 
 
 
@@ -27,9 +42,5 @@ def get_coordinates(ip: ipv4_address) -> dict:
     return clean_response
 
 
-
 def post_h(data : dict):
-    response = requests.post(f'http://db-host/redis', json=data)
-
-
-# response.status_code
+    response = requests.post(f'{service_b_url}', json=data)
