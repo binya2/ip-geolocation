e33adad1c247ebd6ac8e6cb2bc894d288b804a25
Benyamin Shoraki <benyaminchor@gmail.com>
2026-01-13 09:35:37 +0200

    Refactor Redis storage functions to use async/await; fix port environment variable and improve error handling

--- service-b/app/storage.py
+++ service-b/app/storage.py
@@ -1,7 +1,7 @@
 import json
 import os
 
-import redis
+import redis.asyncio as redis
 
 from schemas import IpToCoordinates
 
@@ -9,33 +9,35 @@ INDEX_KEY = "index:all_ips"
 
 r = redis.Redis(
     host=os.getenv("REDIS_HOST", 'localhost'),
-    port=os.getenv("REDIS_POTT", 6379),
+    port=int(os.getenv("REDIS_PORT", 6379)),
     db=0,
-    decode_responses=True)
+    decode_responses=True
+)
 
 
-def save_location(data: IpToCoordinates):
+async def save_location(data: IpToCoordinates):
     try:
         key = f"geo:{data.ip}"
         json_value = data.model_dump_json(include={"coord"})
+
         pipe = r.pipeline()
         pipe.set(key, json_value)
         pipe.sadd(INDEX_KEY, str(data.ip))
-        pipe.execute()
+        await pipe.execute()
         return True
     except Exception as e:
+        print(f"Error saving: {e}")
         return False
 
 
-def get_location_by_ip(ip_address: str) -> IpToCoordinates | None:
+async def get_location_by_ip(ip_address: str) -> IpToCoordinates | None:
     key = f"geo:{ip_address}"
-    raw_json = r.get(key)
+    raw_json = await r.get(key)
     if not raw_json:
         return None
     try:
         data_dict = json.loads(raw_json)
         data_dict["ip"] = ip_address
-        print(data_dict)
         obj = IpToCoordinates.model_validate(data_dict)
         return obj
     except Exception as e:
@@ -43,12 +45,14 @@ def get_location_by_ip(ip_address: str) -> IpToCoordinates | None:
         return None
 
 
-def get_all_locations() -> list[IpToCoordinates]:
-    all_ips = r.smembers(INDEX_KEY)
+async def get_all_locations() -> list[IpToCoordinates]:
+    all_ips = await r.smembers(INDEX_KEY)
     if not all_ips:
         return []
+
     keys = [f"geo:{ip}" for ip in all_ips]
-    json_values = r.mget(keys)
+    json_values = await r.mget(keys)
+
     results = []
     for ip, raw_json in zip(all_ips, json_values):
         if raw_json:
@@ -57,6 +61,6 @@ def get_all_locations() -> list[IpToCoordinates]:
                 data_dict["ip"] = ip
                 obj = IpToCoordinates.model_validate(data_dict)
                 results.append(obj)
-            except Exception as e:
-                print(f"Error parsing IP {ip}: {e}")
+            except Exception:
+                continue
     return results
