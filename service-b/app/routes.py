05c8192c1325f1e1ea7f529692ec5b9443e33a03
Benyamin Shoraki <benyaminchor@gmail.com>
2026-01-13 09:39:27 +0200

    Refactor Redis route handlers to use async/await for improved performance

--- service-b/app/routes.py
+++ service-b/app/routes.py
@@ -9,8 +9,7 @@ router = APIRouter(tags=["Coordinates_api"])
 
 @router.post("/redis", status_code=status.HTTP_201_CREATED)
 async def post(ip_to_coordinates: IpToCoordinates):
-    print(ip_to_coordinates)
-    if storage.save_location(ip_to_coordinates):
+    if await storage.save_location(ip_to_coordinates):
         return {"success": True,
                 "message": "Redis Saved Successfully"}
     else:
@@ -18,17 +17,17 @@ async def post(ip_to_coordinates: IpToCoordinates):
                 "message": "Redis Not Saved Successfully"}
 
 
-@router.get("/redis",status_code=status.HTTP_200_OK)
+@router.get("/redis", status_code=status.HTTP_200_OK)
 async def get_all():
     try:
-        data = storage.get_all_locations()
+        data = await storage.get_all_locations()
         print(data)
         return data
     except Exception as e:
         return {"message": str(e)}
 
 
-@router.get("/redis/{ip}",status_code=status.HTTP_200_OK)
+@router.get("/redis/{ip}", status_code=status.HTTP_200_OK)
 async def get_py_ip(ip):
-    data = storage.get_location_by_ip(ip.__str__())
+    data = await storage.get_location_by_ip(ip.__str__())
     return data
