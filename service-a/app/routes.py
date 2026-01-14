4dc28019b42691eeb1bada51bc15f83c3e7cca93
Benyamin Shoraki <benyaminchor@gmail.com>
2026-01-14 16:20:01 +0200

    Update service configurations and enhance data handling in IP processing

--- service-a/app/routes.py
+++ service-a/app/routes.py
@@ -12,20 +12,18 @@ def get_details_from_ip(ip):
         if response.get('connection'):
             clean_response = clean_data(response)
             is_saved = save_ip_data(clean_response)
+            print("is_saved", is_saved)
+            return {'ip details': clean_response}
         else:
             raise HTTPException(status_code=500, detail={'Error': 'error when trying to save ip in the DB.'})
     except Exception as e:
         raise HTTPException(status_code=500, detail=str(e))
 
-    return {'message': 'ip saved successfully', 'request details': is_saved, 'ip details': clean_response}
-
 
 @router.get('/get-all-ips')
 def get_all():
     try:
-        print("get_all_ips1")
         data = get_all_data()
-        print(data)
         if not data:
             return {'message': 'No data found'}
         return data
--- service-a/app/services.py
+++ service-a/app/services.py
@@ -11,20 +11,24 @@ API_KEY = os.getenv('API_KEY', "5F67eMEeTFUlPaewp4z6JeMQsu83klkB")
 ip2loc = 'https://api.ip2loc.com'
 ip2loc_url = f'{ip2loc}/{API_KEY}'
 service_b_ip = os.getenv('SERVICE_B_IP', "127.0.0.1")
-service_b_port = os.getenv('SERVICE_B_PORT', 61982)
+# service_b_port = os.getenv('SERVICE_B_PORT', 38854)
+service_b_port = 38854
 service_b_url = f'http://{service_b_ip}:{service_b_port}/redis'
 
 
 def clean_data(data: dict) -> IpToCoordinates:
     connection_info = data.get('connection', {})
     location_info = data.get('location', {})
+    longitude = location_info.get('longitude')
+    latitude = location_info.get('latitude')
     ip_str = connection_info.get('ip')
     if not ip_str:
         raise ValueError("Missing IP address in data")
     try:
-        current_ip:IPvAnyAddress = ip_str
-        location = Coordinate(**location_info)
-        return IpToCoordinates(ip=current_ip, coord=location)
+        current_ip: IPvAnyAddress = ip_str
+        location = Coordinate(longitude=longitude, latitude=latitude)
+        data = IpToCoordinates(ip=current_ip, coord=location)
+        return data
     except ValidationError as e:
         print(f"Validation error: {e}")
         raise e
@@ -51,10 +55,9 @@ def get_coordinates(ip: Ip):
 
 
 def save_ip_data(data: IpToCoordinates):
-    coordinates = data.get('coord')
-    if coordinates.get('latitude') and coordinates.get('longitude'):
+    if not (data.coord.latitude is None or data.coord.longitude is None):
         try:
-            response = requests.post(service_b_url, json=data)
+            response = requests.post(service_b_url, json=data.model_dump(mode='json'))
             if response.json().get('success'):
                 raise requests.exceptions.HTTPError(response=response.json().get('message'))
             response.raise_for_status()
